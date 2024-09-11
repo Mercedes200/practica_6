16f0cafcd4a77e6042eb0e264f93262273fc3908
root <root@debian12.myguest.virtualbox.org>
2024-09-11 16:29:25 -0600

    GUIA 6

---
+++ Egema/app/models/estudiante.rb
@@ -0,0 +1,5 @@
+class Estudiante < ApplicationRecord
+
+  validates :nombres, :apellidos, :carrera, :carnet, :fecha_de_nacimiento, :edad, :celular, presence: true
+  validates :celular, numericality: { only_integer: true }, length: { is: 8 }
+end
